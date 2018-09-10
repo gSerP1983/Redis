4f59fe1d5547022e27fbaacbb13780a96f12acfb
Sergey Petrov <gserp1983@gmail.com>
2018-09-10 17:36:06 +0300

    1) TestExpire1 and TestExpire2 2) TestFireAndForget

--- Test.Redis/DbTest.cs
+++ Test.Redis/DbTest.cs
@@ -52,20 +52,49 @@ namespace Test.Redis
         }
 
         [TestMethod]
-        public void TestExpire()
+        public void TestExpire1()
         {
             var redis = ConnectionMultiplexer.Connect("localhost");
             var db = redis.GetDatabase();
 
             var key = Guid.NewGuid().ToString();
 
-            db.StringSet(key, "Expire Test");
-            db.KeyExpire(key, TimeSpan.FromSeconds(0.1), CommandFlags.FireAndForget);
-            Assert.AreEqual((string)db.StringGet(key), "Expire Test");
+            db.StringSet(key, "Expire Test1");
+            db.KeyExpire(key, TimeSpan.FromSeconds(0.1));
+            Thread.Sleep(10);
+            Assert.AreEqual((string)db.StringGet(key), "Expire Test1");
             Assert.IsNotNull(db.StringGetWithExpiry(key).Expiry);
-
-            Thread.Sleep(150);
+            Thread.Sleep(100);
             Assert.IsNull((string)db.StringGet(key));
+        }
+
+        [TestMethod]
+        public void TestExpire2()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase();
+
+            var key = Guid.NewGuid().ToString();
+
+            db.StringSet(key, "Expire Test2", TimeSpan.FromSeconds(0.1));
+            Thread.Sleep(10);
+            Assert.AreEqual((string)db.StringGet(key), "Expire Test2");
+            Assert.IsNotNull(db.StringGetWithExpiry(key).Expiry);
+            Thread.Sleep(100);
+            Assert.IsNull((string)db.StringGet(key));
+        }
+
+        [TestMethod]
+        public void TestFireAndForget()
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost");
+            var db = redis.GetDatabase();
+
+            var key = Guid.NewGuid().ToString();
+            Assert.IsNull((string)db.StringGet(key));
+            db.StringSet(key, "fire and forget", flags: CommandFlags.FireAndForget);
+            Thread.Sleep(10);
+            Assert.AreEqual((string)db.StringGet(key), "fire and forget");
         }
 
         [TestCleanup]
